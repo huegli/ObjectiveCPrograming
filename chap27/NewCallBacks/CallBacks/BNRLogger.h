1560cc9961d30187fc7cf63b6fe124373fa8ab65
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2023-10-12 14:00:02 -0700

    Chapter 27 again

---
+++ chap27/NewCallBacks/CallBacks/BNRLogger.h
@@ -0,0 +1,23 @@
+//
+//  BNRLogger.h
+//  CallBacks
+//
+//  Created by Nikolai Schlegel on 10/11/23.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BNRLogger : NSObject
+        <NSURLConnectionDelegate, NSURLConnectionDataDelegate>
+{
+    NSMutableData *_incomingData;
+}
+
+@property (nonatomic) NSDate *lastTime;
+- (NSString *)lastTimeString;
+- (void)updateLastTime:(NSTimer *)t;
+@end
+
+NS_ASSUME_NONNULL_END
